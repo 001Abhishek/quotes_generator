7a6c5fd40d3c30ce9866495767433d3f9aa2458b
001Abhishek <aprajput321@gmail.com>
2021-01-16 20:09:23 +0530

    consumes api and displays quotes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, useEffect, useState } from 'react';
+import "./App.css";
+import axios from "axios";
 
-function App() {
+
+const App = () => {
+  const [quote, setQuote] = useState("");
+  const[author, setAuthor] = useState("");
+
+  const quoteAPI = async() => {
+    let arrayOfQuotes = [];
+    try {
+      const data = await axios.get(" https://api.quotable.io/random");
+      console.log(data);
+      arrayOfQuotes=data.data;
+      
+    } catch (error) {
+      console.log(error);
+      
+    }
+    try {
+      setQuote(arrayOfQuotes.content);
+      setAuthor(arrayOfQuotes.author);
+    } catch (error) {
+      console.log(error);
+      
+    }
+
+  };
+
+  const tweetQuote = () => {
+    let tweetPost = `https://twitter.com/intent/tweet?text=${quote} ~ ${author}`;
+    window.open(tweetPost);
+  }
+
+  const whatsAppQuote = () => {
+    let whatsAppPost = `whatsapp://send?text=${quote} ~ ${author}`;
+    window.open(whatsAppPost);
+
+  }
+
+  useEffect(() => {
+    quoteAPI();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className="App">
+    <div className="quoteBox">
+      <div className="Container">
+        <div className="quote"><h3>{quote}</h3></div>
+        <div className="author">~{author}</div>
+        <div className="quoteButton"> <button onClick={quoteAPI}>Generate New Quote</button></div>
+        <div className ="twitterButton"><button  onClick={tweetQuote}>Tweet</button></div>
+        <div className ="whatsAppButton"><button  onClick={whatsAppQuote}>WhatsApp</button></div>
+
+        
+
+      </div>
     </div>
-  );
+  </div>
+  )
 }
 
 export default App;
